32a59036fd39a94eb27e910eefc007cfdd1dceaa
Trion129 <parmsingh129@gmail.com>
2025-05-23 17:42:51 +0530

    Add flag for window on top

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     
     MainWindow mainWindow;
+    mainWindow.setWindowFlags(mainWindow.windowFlags() | Qt::WindowStaysOnTopHint);
     mainWindow.show();
     
     return app.exec();
