80a99ab62070acfa647d61369ddd044562fed7ec
Trion129 <parmsingh129@gmail.com>
2025-05-23 17:53:27 +0530

    Detach the cmd while running on windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <QApplication>
 #include "mainwindow.h"
 
+#ifdef _WIN32
+  #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#endif
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     
